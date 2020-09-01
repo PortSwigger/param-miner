17e85e98dbc97e21427297cf0a7090f9e4be6e9a
James <james.kettle@portswigger.net>
2020-09-01 15:38:45 +0100

    Bump version

--- src/burp/BurpExtender.java
+++ src/burp/BurpExtender.java
@@ -23,7 +23,7 @@ import static burp.Keysmith.getWords;
 
 public class BurpExtender implements IBurpExtender, IExtensionStateListener {
     private static final String name = "Param Miner";
-    private static final String version = "1.20";
+    private static final String version = "1.21";
     private ThreadPoolExecutor taskEngine;
     public static List<Scan> scans = new ArrayList<>();
 
