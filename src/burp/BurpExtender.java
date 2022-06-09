a882605c55a5df0c5f59d77f672f34a9a582d6df
James Kettle <albinowax@gmail.com>
2022-06-09 09:37:02 +0100

    Move ParamInsertionPoint into albinowaxUtils

--- src/burp/BurpExtender.java
+++ src/burp/BurpExtender.java
@@ -195,54 +195,6 @@ class RequestWithOffsets {
     }
 }
 
-class ParamInsertionPoint implements IScannerInsertionPoint {
-    byte[] request;
-    String name;
-    String value;
-    byte type;
-
-    ParamInsertionPoint(byte[] request, String name, String value, byte type) {
-        this.request = request;
-        this.name = name;
-        this.value = value;
-        this.type = type;
-    }
-
-    String calculateValue(String unparsed) {
-        return unparsed;
-    }
-
-    @Override
-    public String getInsertionPointName() {
-        return name;
-    }
-
-    @Override
-    public String getBaseValue() {
-        return value;
-    }
-
-    @Override
-    public byte[] buildRequest(byte[] payload) {
-        IParameter newParam = Utilities.helpers.buildParameter(name, Utilities.encodeParam(Utilities.helpers.bytesToString(payload)), type);
-        return Utilities.helpers.updateParameter(request, newParam);
-    }
-
-    @Override
-    public int[] getPayloadOffsets(byte[] payload) {
-        //IParameter newParam = Utilities.helpers.buildParameter(name, Utilities.encodeParam(Utilities.helpers.bytesToString(payload)), type);
-        return new int[]{0, 0};
-        //return new int[]{newParam.getValueStart(), newParam.getValueEnd()};
-    }
-
-    @Override
-    public byte getInsertionPointType() {
-        return type;
-        //return IScannerInsertionPoint.INS_PARAM_BODY;
-        // return IScannerInsertionPoint.INS_EXTENSION_PROVIDED;
-    }
-}
-
 class ParamNameInsertionPoint extends ParamInsertionPoint {
     String attackID;
     String defaultPrefix;
