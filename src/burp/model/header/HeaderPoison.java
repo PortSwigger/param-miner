b7f50fe941f71adca3d84882fd2f88a534a82cff
Anthony Hanel <ahanel13@gmail.com>
2024-04-20 16:25:55 -0600

    - ParamGrabber is now passed in instead of statically access, I prefer since it creates a clear path from start of Extension to here.

--- src/burp/model/header/HeaderPoison.java
+++ src/burp/model/header/HeaderPoison.java
@@ -2,24 +2,28 @@ package burp.model.header;
 
 import java.util.List;
 
-import burp.BurpExtender;
 import burp.IHttpRequestResponse;
 import burp.IScanIssue;
+import burp.model.param.ParamGrabber;
 import burp.model.param.ParamGuesser;
 import burp.model.scanning.BulkScanLauncher;
 import burp.model.scanning.Scan;
 import burp.model.utilities.Utilities;
+import burp.view.SettingsBox;
 
 public class HeaderPoison extends Scan {
 
-    public HeaderPoison(String name, Utilities utilities, BulkScanLauncher launcher) {
+    public HeaderPoison(String name, Utilities utilities, BulkScanLauncher launcher, SettingsBox guessSettings, ParamGrabber paramGrabber) {
         super(name, utilities, launcher);
-        scanSettings.importSettings(BurpExtender.guessSettings);
+        scanSettings.importSettings(guessSettings);
+        this.paramGrabber = paramGrabber;
     }
 
     @Override
     public List<IScanIssue> doScan(IHttpRequestResponse req) {
-        new ParamGuesser(req, false, Utilities.PARAM_HEADER, BurpExtender.paramGrabber, null, 2147483647, utilities.globalSettings, utilities).run();
+        new ParamGuesser(req, false, Utilities.PARAM_HEADER, paramGrabber, null, 2147483647, utilities.globalSettings, utilities).run();
         return null;
     }
+
+private final ParamGrabber paramGrabber;
 }
