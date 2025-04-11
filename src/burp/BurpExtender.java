ae3c2b890c108dd6840b321e01ace020a3c9c06f
Hannah Law <hannah.law@portswigger.net>
2025-04-11 17:04:38 +0000

    BApp Store update v1.55

--- src/burp/BurpExtender.java
+++ src/burp/BurpExtender.java
@@ -18,7 +18,7 @@ import java.util.concurrent.*;
 
 public class BurpExtender implements IBurpExtender, IExtensionStateListener, BurpExtension {
     private static final String name = "Param Miner";
-    private static final String version = "1.54";
+    private static final String version = "1.55";
     private ThreadPoolExecutor taskEngine;
     static ParamGrabber paramGrabber;
     static SettingsBox configSettings = new SettingsBox();
